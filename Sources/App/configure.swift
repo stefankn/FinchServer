60137b53316d5104e20782f62ea15d985ab6ddb1
Stefan <1708541+stefankn@users.noreply.github.com>
2025-02-02 10:04:10 +0100

    Log file locations

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -9,8 +9,15 @@ public func configure(_ app: Application) async throws {
         throw BeetsError.missingBeetsExecutableEnvironmentVariable
     }
     
+    app.logger.notice("Beets executable path: \(beetsExecutablePath)")
+    
     let beetsConfigLocation = try BeetsCLI.getBeetsConfigLocation(executablePath: beetsExecutablePath)
+    
+    app.logger.notice("Beets config location: \(beetsConfigLocation)")
+    
     let beetsDatabaseLocation = try BeetsCLI.getBeetsDatabaseLocation(configURL: beetsConfigLocation)
+    
+    app.logger.notice("Beets database location: \(beetsDatabaseLocation)")
     
     app.beetsConfiguration = BeetsConfiguration(
         executablePath: beetsExecutablePath,
